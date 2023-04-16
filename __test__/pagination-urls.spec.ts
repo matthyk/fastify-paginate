2b776c8144e44fe8ed526b76f9ab79029a820da4
matthyk <53833818+matthyk@users.noreply.github.com>
2023-04-16 16:06:30 +0200

    add test case and fix variable naming

--- __test__/pagination-urls.spec.ts
+++ __test__/pagination-urls.spec.ts
@@ -54,9 +54,44 @@ describe('paginationUrls decorator', function () {
     expect(body.self).toBeDefined()
     expect(body.next).toBeDefined()
 
-    const selfUrl = new URLSearchParams(body.next)
+    const nextUrl = new URLSearchParams(body.next)
 
-    expect(selfUrl.get('offset')).toBe('30')
-    expect(selfUrl.get('limit')).toBe('20')
+    expect(nextUrl.get('offset')).toBe('30')
+    expect(nextUrl.get('limit')).toBe('20')
+  })
+
+  it('should build relative url correctly', async () => {
+    const fastify = Fastify()
+    await fastify.register(fastifyPaginate)
+
+    fastify.get('/posts', async function (req: FastifyRequest) {
+      return req.paginationUrls({
+        self: { offset: 100, limit: 50 },
+        prev: { offset: 50, limit: 50 },
+      })
+    })
+
+    const result = await fastify.inject({
+      method: 'GET',
+      url: '/posts?offset=100&limit=50&name=matthyk&tag=food',
+    })
+
+    const body = JSON.parse(result.body)
+
+    expect(body.self).toBeDefined()
+    expect(body.prev).toBeDefined()
+
+    const selfUrl = new URLSearchParams(body.self)
+    const nextUrl = new URLSearchParams(body.prev)
+
+    expect(selfUrl.get('offset')).toBe('100')
+    expect(selfUrl.get('limit')).toBe('50')
+    expect(selfUrl.get('name')).toEqual('matthyk')
+    expect(selfUrl.get('tag')).toEqual('food')
+
+    expect(nextUrl.get('offset')).toBe('50')
+    expect(nextUrl.get('limit')).toBe('50')
+    expect(nextUrl.get('name')).toEqual('matthyk')
+    expect(nextUrl.get('tag')).toEqual('food')
   })
 })
